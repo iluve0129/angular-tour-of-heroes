75cdbd9ea27641ad7da5e097e914760fb10610e7
yusuke.irube <iluve0129@gmail.com>
2022-12-30 21:33:25 +0900

    0002

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../service/hero.service';
+import { MessageService } from '../service/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,15 +12,25 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  // heroes = HEROES;
+  heroes: Hero[] = [];
   
-  constructor() { }
+  constructor(private heroService: HeroService, 
+              private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
+  }
 
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+  
+  getHeroes(): void {
+    this.heroService.getHeroesFromMock().subscribe(heroes => this.heroes = heroes);;
   }
 
 }
+
